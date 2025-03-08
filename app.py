d3bae861c3fe62a6cddafb8c33fe3d3e22abfa26
Shivam Singh <ssingh070903@gmail.com>
2025-03-08 15:36:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
