0fbd07ac466256c3f838c0974d0bf1cee9ea0ea4
Shiva Singh Pal <your-email@example.com>
2025-03-11 14:21:34 +0530

    updatd

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
